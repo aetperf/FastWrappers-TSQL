75f9d3e691892b4bd6b3156e0c8943cd3b17ef91
rferraton <16419423+rferraton@users.noreply.github.com>
2025-04-03 00:35:24 +0200

    update framework to 4.8.1 (for security) and change version to 0.2.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@
 //     Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
